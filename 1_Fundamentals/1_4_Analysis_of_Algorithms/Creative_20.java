7482ca8504b4c66b6ff44f4cee8540049dea9613
patb-github <165882561+patb-github@users.noreply.github.com>
2024-05-19 16:16:38 +0700

    Optimize indexOfMax and remove array length restriction for input

--- 1_Fundamentals/1_4_Analysis_of_Algorithms/Creative_20.java
+++ 1_Fundamentals/1_4_Analysis_of_Algorithms/Creative_20.java
@@ -3,8 +3,6 @@ public class Creative_20 {
      * An array is bitonic if it is comprised of an increasing sequence of integers followed immediately by a decreasing sequence of integers. 
      * Write a program that, given a bitonic array of n distinct int values, determines whether a given integer is in the array. 
      * Your program should use ~ 3 log n compares in the worst case.
-     * 
-     * Assumption: bitonic arrays have at least 3 elements.
      */
     private Creative_20() { };
 
@@ -12,16 +10,16 @@ public class Creative_20 {
     private static int indexOfMax(int[] arr) {
         int lo = 0;
         int hi = arr.length - 1;
-        int mid = (lo + hi) / 2;
+        int mid = (lo + hi + 1) / 2;    // +1 because we will access mid - 1; for when array has length 2
 
-        while (lo <= hi) 
+        while (lo < hi) 
         {
-            if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) return mid;
-            else if (arr[mid - 1] < arr[mid] && arr[mid] < arr[mid + 1]) lo = mid + 1; // mid is in the increasing sequence, so max is to the right
-            else hi = mid - 1;                                                         // mid is in the decreasing sequence, so max is to the left                               
-            mid = (lo + hi) / 2;
+            // lo will always be on the increasing part, and hi will always be on the decreasing part
+            if (arr[mid - 1] < arr[mid]) lo = mid;          // arr[mid] could be the max, so we cannot set lo = mid + 1 
+            else hi = mid - 1;                              // arr[mid] cannot be the max, so we can set hi = mid - 1                            
+            mid = ((lo + hi) / 2);
         }
-        return -1;
+        return mid;
     }
 
     private static int binarySearch(int[] arr, int key, int low, int high, boolean increasing) {
@@ -47,7 +45,6 @@ public class Creative_20 {
      * @return the index of {@code key} in {@code arr} if it exists; -1 otherwise
      */
     public static int indexOf(int[] arr, int key) {
-        if (arr.length < 3) throw new IllegalArgumentException("Array must have at least 3 elements");
         int inflectionIndex = indexOfMax(arr);
 
         // Since we assume that all array elements are distinct, we can search both sides and guarantee at most one index is not -1;
