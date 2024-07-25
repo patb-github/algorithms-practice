ea95ba3b615bc739a7ee275fcd3bc652a6787ac4
Pat <patb.software@gmail.com>
2024-07-25 18:06:10 +0700

    Implement BST and preorder traversal method for Web_2

---
+++ 3_Searching/3_2_Binary_Search_Trees/Web_2.java
@@ -0,0 +1,88 @@
+/*
+ * 2. BST reconstruction. Given the preorder traversal of a BST (not including null nodes), reconstruct the tree.
+ * 
+ * A preorder traversal is done by visiting the root, then the left subtree, and then the right subtree.
+ */
+
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Web_2 {
+    
+    public static class ReconstructedBST<Key extends Comparable<Key>> {
+        private Node root; 
+        private class Node {
+            private Key key;
+            private Node left, right;
+
+            private Node(Key key) { 
+                this.key = key; 
+            }
+        }
+        
+        // Constructs the bst from the preorder traversal
+        public ReconstructedBST(Iterable<Key> preorder) {
+            root = null;
+            // for (Key key : preorder) {
+            //     if (root == null) root = new Node(key);   
+            //     else {
+
+            //     }
+            // }
+            for (Key key : preorder) put(key);
+        };
+
+        public void put(Key key) {
+            if (root == null) {
+                root = new Node(key);
+                return;
+            }
+    
+            Node current = root;
+            while (current != null) {
+                int cmp = key.compareTo(current.key);
+                if (cmp == 0) return;
+                else if (cmp < 0) { 
+                    if (current.left == null) {
+                        current.left = new Node(key);
+                        return;
+                    }
+                    current = current.left;
+                } 
+                else {
+                    if (current.right == null) {
+                        current.right = new Node(key);
+                        return;
+                    }
+                    current = current.right;
+                } 
+            }
+        }
+
+        private void preorderTraversal(List<Key> queue, Node node) {
+            if (node == null) return;
+            
+            queue.add(node.key);
+            preorderTraversal(queue, node.left);
+            preorderTraversal(queue, node.right);
+        }
+
+        // perform preorder traversal for checking
+        public Iterable<Key> preorderTraversal() {
+            Iterable<Key> queue = new ArrayList<>();
+            
+            return queue;
+        }
+
+    }
+    public static void main(String[] args) {
+        // ReconstructedBST<Integer> bst = new ReconstructedBST<>();
+        int[] preorderArray = {100, 20, 10, 30, 200, 150, 300};
+        ArrayList<Integer> preorder = new ArrayList<>();
+        for (int x : preorderArray) preorder.add(x);
+        System.out.println(preorder);
+
+    }
+
+}
